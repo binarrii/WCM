53aed4ecbd13e73cb20f4d275f751d49ac63fb04
binarrii <karl50018@gmail.com>
2026-04-14 11:22:24 +0800

    optim

--- api/main.py
+++ api/main.py
@@ -15,9 +15,9 @@ def create_app():
     # Enable CORS
     @app.after_request
     def add_cors_headers(response):
-        response.headers["Access-Control-Allow-Origin"] = "*"
         response.headers["Access-Control-Allow-Methods"] = "GET, POST, OPTIONS"
-        response.headers["Access-Control-Allow-Headers"] = "Content-Type"
+        response.headers["Access-Control-Allow-Origin"] = "*"
+        response.headers["Access-Control-Allow-Headers"] = "*"
         return response
 
     # Register blueprints
